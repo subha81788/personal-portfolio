1f20c3b8f5da3356fb566fc9decd019a9f1b00f1
subha81788 <subha81788@gmail.com>
2023-08-03 11:17:26 +0530

    Updated social media urls

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -5,11 +5,19 @@ import {AiOutlineTwitter} from 'react-icons/ai'
 const data = [
   {
     id: 1,
-    link: "https://www.linkedin.com",
+    link: "https://www.linkedin.com/in/subhashis-nath-a6a2a624",
     icon: <AiFillLinkedin />,
   },
-  { id: 2, link: "https://github.com", icon: <AiFillGithub /> },
-  { id: 3, link: "https://twitter.com", icon: <AiOutlineTwitter /> },
+  { 
+    id: 2,
+    link: "https://github.com/subha81788",
+    icon: <AiFillGithub />,
+  },
+  {
+    id: 3,
+    link: "https://twitter.com/SubhashisN88",
+    icon: <AiOutlineTwitter />,
+  },
 ];
 
 export default data
